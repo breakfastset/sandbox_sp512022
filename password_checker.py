1b08db561a4e4c237dc0ffe20167f83893736c69
breakfastset <kleng99@gmail.com>
2022-03-30 11:34:41 +0800

    Add docstrings to password checker

--- password_checker.py
+++ password_checker.py
@@ -1,8 +1,17 @@
+""""
+Description: A simple Password Checker
+
+Name: Palmer
+"""
+
+
 def is_valid_password(text):
+    """Check whether a given text has the correct password format"""
     return 8 <= len(text) <= 20
 
 
 def main():
+    """Start program"""
     new_password = "helloworld"
     print(f"{new_password} is a valid password? {is_valid_password(new_password)}")
 
